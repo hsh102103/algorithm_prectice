6ca75997c7716a2c72a80f3e06280823c9d71c37
hsh102103 <54360785+hsh102103@users.noreply.github.com>
2024-11-01 19:12:56 +0900

    [Silver II] Title: 좌표 압축, Time: 1912 ms, Memory: 359440 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,52 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static Object[] sortedArr;
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        
+
         int N = Integer.parseInt(st.nextToken());
-        int[] initArr = new int[N];
-        Set<Integer> set = new HashSet<>();
-        
+        int[] originArr = new int[N];
+        int[] sortedArr = new int[N];
+        Map<Integer, Integer> map = new HashMap<>();
+
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            initArr[i] = Integer.parseInt(st.nextToken());
-            set.add(initArr[i]);
+            originArr[i] = Integer.parseInt(st.nextToken());
+            sortedArr[i] = originArr[i];
         }
-        sortedArr = set.toArray();
         Arrays.sort(sortedArr);
+        int rank = 0;
+        for(int i: sortedArr) {
+            if(!map.containsKey(i)) {
+                map.put(i, rank++);
+            }
+        }
 
-        for (int i = 0; i < N; i++) {
-            int target = initArr[i];
-            sb.append(binarySearch(target)).append(" ");
+        for(int i: originArr) {
+            sb.append(map.get(i)).append(" ");
         }
         System.out.println(sb);
-    }
-
-    /**
-     *
-     * @param target 목표 value
-     * @return target의 index값
-     */
-    private static int binarySearch(int target) {
-        int index = -1;
-        int start = 0;
-        int end = sortedArr.length-1;
-        while (start <= end) {
-            int mid = (start + end) / 2 ;
-            if(target == (Integer)sortedArr[mid]){
-                index = mid;
-                break;
-            } else if (target < (Integer)sortedArr[mid]) {
-              end = mid-1;
-            } else{
-                start = mid+1;
-            }
-
-        }
-        return index;
     }
 }
